1bf86aecedcda721f1066c8ce2fb3c8d75f7718c
François HYVRIER <francois.hyvrier@younited-credit.fr>
2018-06-11 15:35:32 +0200

    Added applications demonstrating scheduling with Azure Service Bus.

---
+++ Scheduling.AzureServiceBus/Younited.Scheduling.Publisher/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace Younited.Scheduling.Publisher
+{
+    /// <summary>
+    /// The purpose of this program is to send scheduled messages to an Azure service bus queue as configured in file 'appsettings.json'.
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var serviceBusConfiguration = configuration.GetSection("ServiceBus");
+                var connectionString = serviceBusConfiguration["ConnectionString"];
+                var queueName = serviceBusConfiguration["QueueName"];
+
+                var queueClient = new QueueClient(connectionString, queueName);
+
+                int messageNumber = 1;
+
+                while (true)
+                {
+                    Console.WriteLine("Press return to schedule a message to become active in 1 minute.");
+                    Console.ReadLine();
+
+                    var body = Encoding.UTF8.GetBytes($"Message #{messageNumber}");
+                    var message = new Message(body);
+                    var scheduleEnqueueTimeUtc = DateTimeOffset.Now.AddMinutes(1);
+
+                    queueClient.ScheduleMessageAsync(message, scheduleEnqueueTimeUtc);
+
+                    messageNumber++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
---
+++ Scheduling.AzureServiceBus/Younited.Scheduling.Subscriber/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Younited.Scheduling.Subscriber
+{
+    public static class Functions
+    {
+        [FunctionName("ConsumeMessage")]
+        public static async Task ConsumeMessage([ServiceBusTrigger("%QueueName%")]BrokeredMessage brokeredMessage, TraceWriter traceWriter)
+        {
+            string content;
+            var body = brokeredMessage.GetBody<Stream>();
+            using (var streamReader = new StreamReader(body, Encoding.UTF8))
+            {
+                content = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+            }
+
+            traceWriter.Info($"Received message: '{content}'.");
+        }
+    }
+}

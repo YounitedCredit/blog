0d5fb10e76581b5c218b306c9b5419dbe6ce952e
Etienne DELATTRE <etienne.delattre@younited-credit.fr>
2020-08-14 17:46:54 +0200

    repository tests. :black_square_button: use only known customer Ids (useful for demo to know customers exist) :black_square_button: Fill in Program.cs :black_square_button: THE WHOLE CHANGE FEED PROJECT

---
+++ DataGenerator.Tests/PurchaseFakerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DataGenerator.Tests
+{
+    public class PurchaseFakerTests
+    {
+        [Fact]
+        public void Should_Create_Purchase_Using_Only_Registered_Fake_CustomerIds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DataGenerator/Purchase.cs
+++ DataGenerator/Purchase.cs
@@ -1,6 +1,15 @@
+using System;
+using Newtonsoft.Json;
+
 namespace DataGenerator
 {
     public class Purchase
     {
+        public Guid FictionalArticleNumber { get; set; }
+        public string ArticleName { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTimeOffset TransactionDate { get; set; }
+        [JsonProperty("id")]
+        public Guid PurchaseId { get; set; }
     }
 }
